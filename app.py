5d7a66a59adb41d04b5d3235afa63ab66af33afa
224g1a0572@srit.ac.in <224g1a0572@srit.ac.in>
2025-07-23 01:33:57 +0530

    Fix start command and add gunicorn to requirements

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -87,4 +88,5 @@ def generate_bill():
                            items=selected_items, total=total, gst=gst, grand_total=grand_total)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host="0.0.0.0", port=port)
